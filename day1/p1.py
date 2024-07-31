6a3ada5cf9f04d78afa1e6aa4ca8458a5cadc16e
Sreejithpooppatta <sreejithpooppatta68@gmail.com>
2024-07-31 13:20:10 +0530

    day1 learning programs and notes

---
+++ day1/p1.py
@@ -0,0 +1,78 @@
+
+# Check if a character is a digit.
+
+# ch = input("Enter the char: ")
+# if (int (ch) >=0 and int (ch) <=9):
+#     print("yes its digit")
+# else:
+#     print("not")
+
+# perfect square
+
+# import math
+# input_num = int(input("enter a number to check it is a perfect square"))
+# root = int(math.sqrt(input_num))
+# store = root * root
+# if store == input_num:
+#     print(f'{input_num} is a prefect square')
+# else:
+#     print(f'{input_num} is not prefect square')
+
+# num1 = 7
+# num2 = 5
+# if num1 < num2 :
+#     print('num1 = ', num1, ' is smallest')
+# else :
+#     print('num2 = ' + str(num2) + 'is smallest')
+
+# ASSIGNMENTS:
+# Day1:
+
+# 2.  Write a program to check if a number is Even
+# num1 = int(input("Enter a number"))
+# if num1 % 2 == 0 :
+#     print('The number is Even')
+# else :
+#         print('The number is odd')
+
+
+# 3. Write a program to convert a lower case letter to upper case
+# char1 = input('Enter a lower case letter')
+# upper_case = char1.upper() 
+# print('upper case :',upper_case)
+
+
+# 4.Check if a letter is vowel or consonant
+# input_char = input('Enter a letter to check if it is a vowel or consonant: ')
+# if input_char in 'aeiou':
+#     print(f'{input_char} is a vowel')
+# else:
+#     print(f'{input_char} is not a vowel')
+
+    
+# 5.  Check if a number is single digit or not
+# input_num = int(input('Enter a number to check if a number is single digit or not '))
+# if input_num < 10 and input_num >0 :
+#     print(f'{input_num}  is single digit ')
+# else:
+#         print(f'{input_num}  is not single digit ')
+
+
+
+# accept the average score from user (integer) and print the result as follows--
+input_score = int(input('Enter the average score')) 
+if input_score in range (0,50):
+    print('fail')
+elif input_score in range (75) :
+    print('Second class')
+elif input_score in range (74,91):
+        print('First class')
+elif input_score in range (90,101):
+        print('Distinction  class')
+else:
+      print('invalid input')
+
+
+
+
+    
---
+++ day1/p2.py
@@ -0,0 +1,58 @@
+# accept the food item number from the user and serve him the food
+
+# food_item = {
+#     1: 'Mysoor special cofee',
+#     2: 'Tamilnadu special dosa',
+#     3: 'Kerala special Sewa',
+#     4: 'Bhopal special poha',
+#     5: 'Bengaluru special upma'
+# }
+# print('Welcome to Rameshwaram cafe')
+# print('1: Cofee 2: Idily 3:Dosa 4: Sewa 5:Upma')
+# Food_item_num = int(input('accept the food item number'))
+# if Food_item_num in range (0, 6):
+#     print('your ordered food '+ food_item.get(Food_item_num) + 'is here'  )
+#     print('Thank you and visit again')
+   
+# else:
+#      print("Invalid input")
+
+
+
+#Accept the food item number from the user and serve him the food.
+import sys
+veg_food_items = { 
+    1 : 'Mysuru Filter Coffee',
+    2 : 'Yummy Idly-vada', 
+    3 : 'Worlds soft Mysuru Mailari Dosa', 
+    4 : 'Bhupal Special Poha', 
+    5 : 'Bengaluru tamato-peanuts Upama' 
+}
+
+non_veg_food_items = { 
+    1 : 'Egg Pokoda',
+    2 : 'Chicken Biryani', 
+    3 : 'Fish Fry', 
+    4 : 'Mutton Masala' 
+} 
+
+food_types = { 
+    1 : veg_food_items, 
+    2 : non_veg_food_items 
+} 
+food_items = { 
+    1 : '1:Coffee 2:Idly-Vada 3:Dosa 4:Poha 5:Upama', 
+    2 : '1:Egg Pokoda 2:Chicken Biryani 3:Fish Fry  4: Mutton Masala'
+} 
+print('Welcome to our hotel The Taste') 
+user_choice = int(input('1:Veg 2:Non-Veg \t Your choice please: '))
+items = food_items.get(user_choice, 'Invalid') 
+if items == 'Invalid': 
+    sys.exit('Invalid choice entered')
+
+print(items) 
+food_item_number = int(input('Enter the food item number that you wish:')) 
+print('Your ' + food_types.get(user_choice) + ' is here') 
+print('Thank you, Visit again')
+
+
