53df0fc06656d1d8ffa35f0a488033d9119e24eb
Sreejithpooppatta <sreejithpooppatta68@gmail.com>
2024-08-01 08:55:24 +0530

    assignment day 2 coding completed

---
+++ day1/p10assigment.py
@@ -0,0 +1,52 @@
+# 7. Print the Fibo series of n terms with 1st 2 terms as 1 and 2.
+
+# input_num = int(input(' Enter the number of terms needed'))
+# first_num = 0
+# second_num = 1
+# for i in range (0,input_num):
+#     print(f'{first_num} ')
+#     third_num = first_num + second_num
+#     first_num = second_num
+#     second_num = third_num
+     
+
+
+
+
+
+
+# 8. Count the number of Prime digits in a number
+
+# list_prime = [2, 3, 7, 5]
+# input_num = input("Enter the digits: ")
+# count = 0
+
+# for i in input_num:
+#     if int(i) in list_prime:
+#         print(f'{i} is a prime number')
+#         count += 1
+#         break
+# print(f'There is {count} prime number')
+
+    
+
+
+
+
+
+
+
+
+# # 9. Print a hollow square of n lines
+# input_row = int(input('Enter the number of rows'))
+# input_col = input_row
+# for i in range(input_row):
+#     for j in range(input_row):
+#         if i == 0 or i == (input_row-1) or j == 0 or j == (input_col-1) : 
+#             print('* ',end='')
+#         else:
+#             print('  ',end='')
+#     print()
+
+
+
---
+++ day1/p8 assigment.py
@@ -0,0 +1,70 @@
+# ==Assigments==
+
+# # 1. Find sum of Even placed digits in a number
+# input_num = (input('Enter the digit '))
+# count = 0
+# sum_even = 0
+# for i in (input_num):
+#     if int(i) % 2 == 0:
+#         count +=1
+#         sum_even += int(i)
+# print(f'The sum of even number is  {sum_even}')
+
+
+
+
+
+
+
+
+# 2. Find biggest digit in a number
+
+# input_num = input('Enter the digit')
+# largest_digit = max(input_num)
+# print(largest_digit)
+
+# input_num = int(input('Enter the digit'))
+# largest_num = 0
+# while input_num > 0:
+#     digit = input_num % 10
+#     if largest_num < input_num :
+#         largest_num = digit
+#     input_num = input_num // 10
+# print(f'largest num in the digit is {largest_num}')
+
+
+
+
+
+
+
+# 3. Find 2nd smallest digit in a number
+
+# input_num = input('Enter the digit: ')
+# smallest_num = float('inf')
+# second_smallest_num = float('inf')
+
+# for i in input_num:
+#     digit = int(i)
+#     if digit < smallest_num:
+#         second_smallest_num = smallest_num
+#         smallest_num = digit
+#     elif digit < second_smallest_num:
+#         second_smallest_num = digit
+# print(f"The second smallest digit in {input_num} is {second_smallest_num}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
