35f7752d0108a512bc78c9ae034a0e0e10f65855
Sreejithpooppatta <sreejithpooppatta68@gmail.com>
2024-08-07 06:49:18 +0530

    day6 updated

--- week2/day1/p1.py
+++ week2/day1/p1.py
@@ -1,12 +1,9 @@
-# str1 = 'sreejith'
-# str2 = 'pooppatta'
-# print(str1,str2,sep=' ')
-
-# str1 = 'Hello'
-# str2 = input('enter')
-
-# try:
-#     print(str1 + ' '+ str2)  
-# except ValueError:
-#     print(f'cannot add an integer and a string.')
+str1 = 'sreejith'
+str2 = ' '
+str3 = 5
+try:
+    concat = str1+str2+str3
+    print(concat)
+except TypeError:
+    print(f'cannot add an integer and a string.')
 
